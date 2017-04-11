ff2541c4cec234809437d607bb0eea7d217eebb2
Mike Howe - Dockter <mike@mcsnetworks.com>
2017-04-11 18:09:44 -0500

    Fix for "ExistingChunkPopulator" trying to do an infinite lookup on the server when checking tileEntityChest.
    Signed-off-by: Mike Howe - Dockter <mike@mcsnetworks.com>

--- src/mod/gcewing/sg/NaquadahOreBlock.java
+++ src/mod/gcewing/sg/NaquadahOreBlock.java
@@ -8,13 +8,11 @@ package gcewing.sg;
 
 import java.util.*;
 
-import net.minecraft.block.*;
 // import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.*;
 import net.minecraft.item.*;
 // import net.minecraft.util.BlockPos;
-
-import net.minecraftforge.common.*;
+import net.minecraft.world.World;
 
 public class NaquadahOreBlock extends BaseOreBlock {
 
@@ -36,5 +34,10 @@ public class NaquadahOreBlock extends BaseOreBlock {
     public int quantityDropped(Random random) {
         return 2 + random.nextInt(5);
     }
-
+    
+    // Almura: added this method to override BaseBlock.java to fix infinite loops within getBlock() and TileEntityChest.
+    @Override
+    public void onBlockAdded(World world, int x, int y, int z) {}
+    
+    // Almura: end.
 }
