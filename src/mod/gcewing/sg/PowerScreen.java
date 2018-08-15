47261a97bae627fa0a1c06ac9251d564150f41e2
Dockter <dockter@almuramc.com>
2018-08-15 00:21:29 -0500

    More house cleaning.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/mod/gcewing/sg/PowerScreen.java
+++ src/mod/gcewing/sg/PowerScreen.java
@@ -18,9 +18,9 @@ public class PowerScreen extends BaseGui.Screen {
 
     final static int guiWidth = 148;
     final static int guiHeight = 64;
-    final static DecimalFormat dFormat = new DecimalFormat("###,###,###");
+    private final DecimalFormat dFormat = new DecimalFormat("###,###,###");
 
-    PowerTE te;
+    private PowerTE te;
     
     public static PowerScreen create(EntityPlayer player, World world, BlockPos pos) {
         PowerContainer container = PowerContainer.create(player, world, pos);
@@ -30,7 +30,7 @@ public class PowerScreen extends BaseGui.Screen {
             return null;
     }
     
-    public PowerScreen(PowerContainer container) {
+    private PowerScreen(PowerContainer container) {
         super(container, guiWidth, guiHeight);
         this.te = container.te;
     }
@@ -42,25 +42,25 @@ public class PowerScreen extends BaseGui.Screen {
 
     protected void drawBackgroundLayer() {
         bindTexture(SGCraft.mod.resourceLocation("textures/gui/power_gui.png"), 148, 64);
-        drawTexturedRect(0, 0, guiWidth, guiHeight, 0, 0);
-        int cx = xSize / 2;
+        drawTexturedRect(0, 0, this.width, this.height, 0, 0);
+        int cx = this.xSize / 2;
         drawCenteredString(te.getScreenTitle(), cx, 8);
         drawRightAlignedString(te.getUnitName()+":", 90, 23);
-        drawRightAlignedString(dFormat.format(Math.min(Math.max(te.energyBuffer, 0), te.energyMax)), 141, 23);
+        drawRightAlignedString(this.dFormat.format(Math.min(Math.max(this.te.energyBuffer, 0), this.te.energyMax)), 141, 23);
         drawRightAlignedString("SGPU:", 90, 34);
-        drawRightAlignedString(dFormat.format(Math.min(Math.max(te.energyBuffer/te.energyPerSGEnergyUnit, 0), te.energyMax)), 141, 34);
+        drawRightAlignedString(this.dFormat.format(Math.min(Math.max(this.te.energyBuffer/this.te.energyPerSGEnergyUnit, 0), this.te.energyMax)), 141, 34);
         drawRightAlignedString("Max:", 90, 45);
-        drawRightAlignedString(dFormat.format(te.energyMax), 141, 45);
+        drawRightAlignedString(this.dFormat.format(this.te.energyMax), 141, 45);
          drawPowerGauge();
     }
     
-    void drawPowerGauge() {
+    private void drawPowerGauge() {
         gSave();
         glPushAttrib(GL_ENABLE_BIT | GL_CURRENT_BIT);
         glEnable(GL_BLEND);
         glBlendFunc(GL_ONE, GL_ONE);
         setColor(1, 0, 0);
-        drawRect(23, 28, 29 * te.energyBuffer / te.energyMax, 10);
+        drawRect(23, 28, 29 * this.te.energyBuffer / this.te.energyMax, 10);
         glBlendFunc(GL_ONE, GL_ZERO);
         glPopAttrib();
         gRestore();
